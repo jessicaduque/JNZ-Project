2813dfb26fe8ec28d26c1a585aa914c534e423e0
jessicaduque <69450467+jessicaduque@users.noreply.github.com>
2023-06-27 18:35:55 -0300

    Conserto checkpoints

--- Assets/Scripts/Pedra.cs
+++ Assets/Scripts/Pedra.cs
@@ -4,37 +4,11 @@ using UnityEngine;
 
 public class Pedra : MonoBehaviour
 {
-    GameObject Player;
-    Vector3 frente;
-    float rapidez = 10f;
     public Vector3 PosicaoInicial;
-    bool podeMover = false;
 
     void Start()
     {
-        Player = GameObject.FindGameObjectWithTag("Player");
         PosicaoInicial = transform.position;
     }
 
-    void Update()
-    {
-        if (podeMover)
-        {
-            frente = Player.GetComponent<Personagem>().frentePedra;
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        if (podeMover)
-        {
-            Player.GetComponent<Rigidbody>().velocity = rapidez * frente;
-        }
-    }
-
-    public void MudarEstadoMovimento()
-    {
-        podeMover = !podeMover;
-    }
-    
 }
