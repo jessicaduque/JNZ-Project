2115ac423d8298b5f80bc060f1335a9d31bd784a
jessicaduque <69450467+jessicaduque@users.noreply.github.com>
2023-06-29 18:09:05 -0300

    Final efeitos visuais de canalização

--- Assets/Scripts/EfeitoVisualCanalizar.cs
+++ Assets/Scripts/EfeitoVisualCanalizar.cs
@@ -12,9 +12,15 @@ public class EfeitoVisualCanalizar : MonoBehaviour
     private float zMin = -8.5f;
     [SerializeField]
     float Zdec;
+    [SerializeField]
+    private CanvasGroup canvasGroup;
+    [SerializeField]
+    float Pretodec;
 
     bool finalizando = false;
 
+    bool cam = false;
+    bool preta = false;
 
     private void Update()
     {
@@ -22,30 +28,60 @@ public class EfeitoVisualCanalizar : MonoBehaviour
         {
             NaoCanalizando();
         }
+        else
+        {
+            preta = false;
+            cam = false;
+        }
     }
 
     public void Canalizando()
     {
+        // Parte da cam
         var transposer = vcam.GetCinemachineComponent<CinemachineTransposer>();
         float zAtual = transposer.m_FollowOffset.z;
-        if(zAtual < zMin)
+        if (zAtual < zMin)
         {
             transposer.m_FollowOffset += new Vector3(0, 0, Zdec) * Time.deltaTime;
+        }
+
+        // Parte da tela preta
+        if (canvasGroup.alpha < 1)
+        {
+            canvasGroup.alpha += Pretodec * Time.deltaTime;
         }
     }
 
     public void NaoCanalizando()
     {
+        // Parte da cam
         finalizando = true;
         var transposer = vcam.GetCinemachineComponent<CinemachineTransposer>();
         float zAtual = transposer.m_FollowOffset.z;
         if (zAtual > zOriginal)
         {
             transposer.m_FollowOffset -= new Vector3(0, 0, Zdec) * Time.deltaTime;
+            cam = false;
         }
         else
         {
             transposer.m_FollowOffset = new Vector3(transposer.m_FollowOffset.x, transposer.m_FollowOffset.y, zOriginal);
+            cam = true;
+        }
+
+        // Parte da tela preta
+        if (canvasGroup.alpha > 0)
+        {
+            preta = false;
+            canvasGroup.alpha -= Pretodec * Time.deltaTime;
+        }
+        else
+        {
+            preta = true;
+        }
+
+        if(preta && cam)
+        {
             finalizando = false;
         }
     }
